8be7ec19e786de9adb65d22c8ea67b8960ec1ff7
unknown <buariadebola@gmail.com>
2025-12-27 17:52:02 +0100

    fix

--- src/Components/SearchResult.jsx
+++ src/Components/SearchResult.jsx
@@ -11,20 +11,23 @@ const SearchResult = (props) => {
       }}
       state={{
         book: props.book,
-        img: props.img,
-        title: props.title,
-        author: props.author,
-        genre: props.genre,
-        price: props.price,
-        bookshelves: props.bookshelves,
+    img: props.img,
+    title: props.title,
+    author: props.author,
+    price: props.price,
+    bookId,
+    bookshelves: props.bookshelves,
         downloads: props.downloads,
-        bookId,
-        description:
+    // This will handle both string and object description
+    description:
       typeof props.description === "string"
         ? props.description
         : props.description && props.description.value
           ? props.description.value
           : "",
+    bookKey: props.bookKey || props.id || '1' + Math.random().toString(36).substr(2, 9),
+    genre: props.genre || 'Unknown',
+    contentUrl: props.contentUrl || null,
       }}>
         <div className="search-result">
           <img src={props.img} alt="" />
